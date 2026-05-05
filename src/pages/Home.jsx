9de7daa1c3d53e8f2a51fd27d0a28504a9462223
SonponYe-shuaChief <sonponyeshua@gmail.com>
2026-05-05 22:17:47 +0000

    fixed usstate error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import { Link, useState, useEffect } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import Button from "../components/common/Button";
 import useCryptoData from "../hooks/useCryptoData";
 import { cryptoApi, normalizeCryptoCollection } from "../api/client";
